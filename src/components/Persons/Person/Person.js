b00bce4ed165279d3fdaa82fca17dece84e497fa
Guilherme-Valle <guidovalle1608@gmail.com>
2020-07-29 20:21:30 -0300

    Adiciona novo tipo de high order component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Aux';
+import withClass from '../../../hoc/withClass';
+
 
 class Person extends Component {
 
   render() {
     return (
-      <div className={classes.Person}>
+      <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and i am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed} value={this.props.name} />
-      </div>
+      </Aux>
     )
   }
 
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import Aux from '../hoc/Aux';
+import withClass from '../hoc/withClass';
 
 class App extends Component {
 
@@ -85,7 +87,7 @@ class App extends Component {
         );
     }
     return (
-      <div className={classes.App}>
+      <Aux>
         <button onClick={() => this.setState({ showCockpit: false })}> Cockpit button </button>
         {
           this.state.showCockpit ? <Cockpit
@@ -94,9 +96,9 @@ class App extends Component {
             personsLength={this.state.persons.length}
             clicked={this.togglePersonsRender} /> : null}
         {persons}
-      </div>
+      </Aux>
     );
   }
 }
 
-export default App;
+export default withClass(App, classes.App);
